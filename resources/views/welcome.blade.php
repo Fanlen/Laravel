b8704170447856404a6191189c7e342c2e9393e6
FanLen <70324486@qq.com>
2016-12-26 07:34:07 +0000

    开始测试

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5 - by Fanlen</div>
+                <div class="title">Laravel 5 - by Fanlen Modify</div>
             </div>
         </div>
     </body>
